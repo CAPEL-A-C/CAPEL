17fa8f88fd833c0e2cc08e56f6acaf2ed6057958
Rudwolf-Dev <rodolfopemc@outlook.com>
2025-10-21 11:12:13 -0600

    Advertencias
    Se añadió una advertencia sobre la modificación de la consola de desarrolladores

--- src/main.js
+++ src/main.js
@@ -18,3 +18,11 @@ app.use(createPinia()).use(router).mount('#app')
 navBar.use(createPinia()).use(router).mount('#navBar')
 
 footBar.use(createPinia()).use(router).mount('#footBar')
+
+let mensajes = new Map()
+mensajes.set("title", "Advertencia")
+mensajes.set("parrafo", "Si estas leyendo esto, recuerda que esta seccion esta diseñada para el uso esclusivo de los desarrolladores; cualquier modificacion en esta pestaña puede arruinar la experiencia del usuario.")
+
+mensajes.forEach((valor) => {
+console.warn(`${valor}`);
+});
